100f7e3757200e46b5c54fec5df51a7d6a723f4e
Miguel de Barros <miguel@debarros.me>
2023-08-17 15:23:17 +0200

    added some output to runner script

--- test/perf/run.sh
+++ test/perf/run.sh
@@ -3,5 +3,9 @@
 # Get the current date and time
 now=$(date +"%Y%m%d-%H%M%S")
 
+echo "Starting test > $now.log"
+
 # Run npm start and pipe the output to a file with the current date and time
 npm start > "$now.log"
+
+echo "Test complete"
