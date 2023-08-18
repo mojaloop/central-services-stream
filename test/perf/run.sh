645030f08f1e51c85a60d7af55082aab35205f21
Miguel de Barros <miguel@debarros.me>
2023-08-18 17:30:46 +0200

    modified run script to use case statement

--- test/perf/run.sh
+++ test/perf/run.sh
@@ -9,21 +9,24 @@ echo "Starting tests - $now" > "./results/$now.log"
 
 echo "=======================================" >> "./results/$now.log"
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "MacOS version: $(sw_vers -productVersion)"  >> "./results/$now.log"
-    echo "MacOS CPU: $(sysctl -n machdep.cpu.brand_string)" >> "./results/$now.log"
-    echo "MacOS Memory: $(sysctl -n hw.memsize | awk '{print $0/1073741824 " GB"}')" >> "./results/$now.log"
-# Check if the OS is Linux
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [ -f /etc/os-release ]; then
+case "$OSTYPE" in
+    darwin*)
+      echo "MacOS version: $(sw_vers -productVersion)"  >> "./results/$now.log"
+      echo "MacOS CPU: $(sysctl -n machdep.cpu.brand_string)" >> "./results/$now.log"
+      echo "MacOS Memory: $(sysctl -n hw.memsize | awk '{print $0/1073741824 " GB"}')" >> "./results/$now.log"
+    ;;
+    linux-gnu*)
+      if [ -f /etc/os-release ]; then
         . /etc/os-release
         echo "Linux distribution: $PRETTY_NAME" >> "./results/$now.log"
-        echo "Linux CPU: $(grep "model name" /proc/cpuinfo | head -n1 | awk -F: '{print $2}' | sed 's/^[ \t]*//')" >> "./results/$now.log"
-        echo "Linux Memory: $(free -h | awk '/^Mem:/ {print $2}')" >> "./results/$now.log"
-    fi
-else
-    echo "Unsupported OS" >> "./results/$now.log"
-fi
+      fi
+      echo "Linux CPU: $(grep "model name" /proc/cpuinfo | head -n1 | awk -F: '{print $2}' | sed 's/^[ \t]*//')" >> "./results/$now.log"
+      echo "Linux Memory: $(free -h | awk '/^Mem:/ {print $2}')" >> "./results/$now.log" 
+    ;;
+    *)
+      echo "Unsupported OS" >> "./results/$now.log"
+    ;;
+esac
 
 echo "=======================================" >> "./results/$now.log"
 
