cc654e8472d9bdbd033a7900e8d3942dd4262669
Steven Oderayi <oderayi@gmail.com>
2023-08-21 16:08:59 +0100

    chore(mojaloop/#3448): update dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,5 @@
 module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
-    "tape"
   ]
 }
