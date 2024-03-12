1407af5ec301461324a7fe6c24f530e9e9a1671d
Kalin Krustev <kalin.krustev@gmail.com>
2024-03-12 13:53:07 +0000

    Add async-exit-hook dependency

--- src/kafka/producer.js
+++ src/kafka/producer.js
@@ -41,10 +41,10 @@ const Logger = require('@mojaloop/central-services-logger')
 const Kafka = require('node-rdkafka')
 const Protocol = require('./protocol')
 
-const connectedClients = new Set();
+const connectedClients = new Set()
 require('async-exit-hook')(callback => Promise.allSettled(
   Array.from(connectedClients).map(client => new Promise(resolve => client.disconnect(resolve)))
-).finally(callback));
+).finally(callback))
 
 /**
  * Producer ENUMs
@@ -293,7 +293,7 @@ class Producer extends EventEmitter {
       })
 
       this._producer.on('disconnected', (metrics) => {
-        connectedClients.delete(this._producer);
+        connectedClients.delete(this._producer)
         Logger.isDebugEnabled && logger.debug(`Producer::onDisconnected - ${JSON.stringify(metrics)}`)
         super.emit('disconnected', metrics)
       })
@@ -320,7 +320,7 @@ class Producer extends EventEmitter {
           Logger.isSillyEnabled && logger.silly('Producer::connect() - end')
           return reject(error)
         }
-        connectedClients.add(this._producer);
+        connectedClients.add(this._producer)
         Logger.isSillyEnabled && logger.silly('Producer::connect() - metadata:')
         Logger.isSillyEnabled && logger.silly(metadata)
         resolve(true)
