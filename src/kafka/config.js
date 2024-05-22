20c920421b3bfd072a467ff6e1cfc06988ee3c24
Kalin Krustev <kalin.krustev@gmail.com>
2024-05-22 09:50:33 +0000

    feat: enable Kafka configuration via the environment

--- src/kafka/config.js
+++ src/kafka/config.js
@@ -1,4 +1,7 @@
+const insertDots = object => Object.fromEntries(Object.entries(object).map(([key, value]) => [key.replace(/[A-Z]/g, s => '.' + s.toLowerCase()), value]))
+
 module.exports = config => config && ({
   ...config,
-  rdkafkaConf: config.rdkafkaConf && Object.fromEntries(Object.entries(config.rdkafkaConf).map(([key, value]) => [key.replace(/[A-Z]/g, s => '.' + s.toLowerCase()), value]))
+  rdkafkaConf: config.rdkafkaConf && insertDots(config.rdkafkaConf),
+  topicConf: config.topicConf && insertDots(config.topicConf)
 })
