92d122a08fee3dd4f0858e1218c5fb7396391972
geka.evk <geka.evk@gmail.com>
2024-10-15 12:48:23 +0100

    feat(csi-107): updated deps

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,7 @@
 module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
-    "tape"
+    "tape",
+    "node-rdkafka" // need to check how major version (3) is handled
   ]
 }
