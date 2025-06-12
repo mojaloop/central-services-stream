2d7bd7b630f185fe3dc2c9efdfa93890e4e86fca
Kevin Leyow <kleyow@gmail.com>
2025-06-11 22:50:54 -0500

    feat: bump kafka

--- .ncurc.js
+++ .ncurc.js
@@ -2,6 +2,5 @@ module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
     "tape",
-    "node-rdkafka" // updating to the next major v3.0.0 should be done in a separate task
   ]
 }
