3db57cb998e1cc162bcefdcacdf8a969e096009d
Kevin Leyow <kleyow@gmail.com>
2025-07-10 09:37:46 -0500

    chore: downgrade for performance

--- .ncurc.js
+++ .ncurc.js
@@ -2,5 +2,7 @@ module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
     "tape",
+    // Performance issues with node-rdkafka v3.4.1, reverting to v3.3.1
+    "node-rdkafka"
   ]
 }
