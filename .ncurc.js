f6c6d5940a7f75ce55741138fbd4b2a049ce3e09
Shashikant Hirugade <shashi.pioneer@gmail.com>
2025-08-06 12:33:33 -0600

    fix: upgrade node-rdkafka

--- .ncurc.js
+++ .ncurc.js
@@ -3,6 +3,6 @@ module.exports = {
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
     "tape",
     // Performance issues with node-rdkafka v3.4.1, reverting to v3.3.1
-    "node-rdkafka"
+    // "node-rdkafka"
   ]
 }
