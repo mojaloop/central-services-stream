0bfe3afabf2b83e41918e76d2d09b0fbc104d3cd
Kevin Leyow <kleyow@gmail.com>
2025-09-08 22:51:01 -0500

    chore: bump node rd kafka

--- .ncurc.js
+++ .ncurc.js
@@ -1,6 +1,7 @@
 module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
-    "tape"
+    "tape",
+    "simple-swizzle", // https://github.com/advisories/GHSA-wwpx-h6g5-c7x6 Malware in v0.2.3
   ]
 }
