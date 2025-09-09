28bb50d50236df31ae24c8576351c7ddbe23098c
Kevin Leyow <kleyow@gmail.com>
2025-09-09 08:55:22 -0500

    chore: bump node rd kafka (#177)
    * chore: bump node rd kafka
    
    * chore: bump node rd kafka

--- .ncurc.js
+++ .ncurc.js
@@ -2,7 +2,6 @@ module.exports = {
   reject: [
     // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
     "tape",
-    // Performance issues with node-rdkafka v3.4.1, reverting to v3.3.1
-    "node-rdkafka"
+    "simple-swizzle", // https://github.com/advisories/GHSA-wwpx-h6g5-c7x6 Malware in v0.2.3
   ]
 }
