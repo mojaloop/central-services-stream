a88895e5044cc3a909bb71eac2f22d5b2a006fcb
Juan Correa <5475265+gibaros@users.noreply.github.com>
2026-07-09 16:04:38 -0400

    chore: bump orb to 2.1.3, node to 24.18.0, update deps and fix vulner… (#195)

--- .ncurc.js
+++ .ncurc.js
@@ -1,8 +1,9 @@
 module.exports = {
   reject: [
-    // TODO: Upgrading tape to v5+ causes tests to fail due to assert.end() being called multiple times. Will need to address this! Perhaps even move to Jest?
+    // tape 5 breaks the suite (verified on 5.10.2, 2026-07): 16/768 failures, all ".end() already called", in the
+    // event-driven Consumer tests (flow/poller/recursive modes). The Kafka stub keeps emitting message/batch events
+    // after a test's plan is fulfilled; tape 4 tolerated the extra assert/end calls, tape 5 fails them hard.
     "tape",
-    "simple-swizzle", // https://github.com/advisories/GHSA-wwpx-h6g5-c7x6 Malware in v0.2.3
-    "eslint" // https://github.com/advisories/GHSA-p5wg-g6qr-c7cgis incompatible with standard@17.1.2. ESLint 9 removed several options (extensions, useEslintrc, resolvePluginsRelativeTo) that standard-engine relies on. The standard package requires eslint@^8.41.0
+    "simple-swizzle" // https://github.com/advisories/GHSA-wwpx-h6g5-c7x6 Malware in v0.2.3
   ]
 }
